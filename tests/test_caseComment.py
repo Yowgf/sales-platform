b76e26b325893009ee8a42557cf974c34e72d9b9
Breno Pimenta <brenocsp@gmail.com>
2021-12-03 15:29:12 -0300

    Update test_caseComment.py

--- tests/test_caseComment.py
+++ tests/test_caseComment.py
@@ -5,17 +5,15 @@ from storageManager.case.caseComment.caseComment import caseComment
 
 class TestCaseComment:
 
-  @pytest.fixture
   def create_newCommmet():
    newComment = caseComment("Leona Vanessa", "Recomendo bastante")
    return newComment
 
-  def test_CanDefineCreatedBy (self, create_newCommmet):
-    newComment = create_newCommmet
-    assert newComment.createdBy == "Leona Vanessa"
+  def test_CanDefineCreatedBy (self):
+    assert TestCaseComment.create_newCommmet().createdBy == "Leona Vanessa"
 
-  def test_CanDefineComment (self, create_newCommmet):
-    assert create_newCommmet.comment == "Recomendo bastante"
+  def test_CanDefineComment (self):
+    assert TestCaseComment.create_newCommmet().comment == "Recomendo bastante"
 
   # def test_CanDefineCommentDatetime (self, createNewComment):
   #   assert createNewComment.newComment.createdAt.formalStr == createNewComment.createdAt.formalStr
