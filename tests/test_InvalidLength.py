c261abd7c07f9d5278134585c0ad949b96fea42c
Breno Pimenta <brenocsp@gmail.com>
2021-12-03 11:54:30 -0300

    Update test_InvalidLength.py

--- tests/test_InvalidLength.py
+++ tests/test_InvalidLength.py
@@ -2,5 +2,8 @@ import pytest
 from errors.InvalidLength import InvalidLength
 
 class TestInvalidLengthExeceptionRaised:
-    with pytest.raises(InvalidLength):
-        InvalidLength("testing value", [0,1])
+    with pytest.raises(Exception):
+      try:
+        False
+      except (ValueError, InvalidLength) as err:
+        InvalidLength("10", [0,1])
