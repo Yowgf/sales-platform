902f2d42465a7776c2f3a4e4227a9a69ed11e67b
Breno Pimenta <brenocsp@gmail.com>
2021-12-03 08:20:07 -0300

    Changes to easy class test

--- tests/test_easyClass.py
+++ tests/test_easyClass.py
@@ -1,36 +1,36 @@
 import pytest
-from easyClass import Stack
+from easyClass import easyClass
 
 class TestStack:
 
   @pytest.fixture
   def setUp(self):
-    self.stack = Stack()
+    self.easyClass = easyClass()
 
   def test_EmptyStack(self):
-    self.assertTrue(self.stack.is_empty())
+    self.assertTrue(self.easyClass.is_empty())
 
   def test_NotEmptyStack(self):
-    self.stack.push(10)
-    self.assertFalse(self.stack.is_empty())
+    self.easyClass.push(10)
+    self.assertFalse(self.easyClass.is_empty())
 
   def test_SizeStack(self):
-    self.stack.push(10)
-    self.stack.push(20)
-    self.stack.push(30)
-    size = self.stack.size
+    self.easyClass.push(10)
+    self.easyClass.push(20)
+    self.easyClass.push(30)
+    size = self.easyClass.size
     self.assertEqual(3, size)
 
   def test_PushPopStack(self):
-    self.stack.push(10)
-    self.stack.push(20)
-    self.stack.push(30)
-    self.stack.pop()
-    result = self.stack.pop()
+    self.easyClass.push(10)
+    self.easyClass.push(20)
+    self.easyClass.push(30)
+    self.easyClass.pop()
+    result = self.easyClass.pop()
     self.assertEqual(20, result)
 
   def test_EmptyStackException(self):
-    self.stack.push(10)
-    self.stack.pop()
+    self.easyClass.push(10)
+    self.easyClass.pop()
     with self.assertRaises(Exception):
-      self.stack.pop()
+      self.easyClass.pop()
