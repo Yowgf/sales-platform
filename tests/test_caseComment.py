93ef96a5160b0535358c95b96303fc9499987e79
Breno Pimenta <brenocsp@gmail.com>
2021-12-03 15:36:47 -0300

    test case comment

--- tests/test_caseComment.py
+++ tests/test_caseComment.py
@@ -1,12 +1,12 @@
 import pytest
 
-# from utils import utils
+from utils import utils
 from storageManager.case.caseComment.caseComment import caseComment
 
 class TestCaseComment:
 
   def create_newCommmet():
-   newComment = caseComment("Leona Vanessa", "Recomendo bastante")
+   newComment = caseComment("Leona Vanessa", "Recomendo bastante", "2021-12-03 18:34:27.041293")
    return newComment
 
   def test_CanDefineCreatedBy (self):
@@ -15,6 +15,5 @@ class TestCaseComment:
   def test_CanDefineComment (self):
     assert TestCaseComment.create_newCommmet().comment == "Recomendo bastante"
 
-  # def test_CanDefineCommentDatetime (self, createNewComment):
-  #   assert createNewComment.newComment.createdAt.formalStr == createNewComment.createdAt.formalStr
-  #   assert createNewComment.newComment.createdAt.prettyStr == createNewComment.createdAt.prettyStr
+  def test_CanDefineCommentDatetime (self, createNewComment):
+    assert TestCaseComment.create_newCommmet().createdAt.dt == "2021-12-01 18:32:45.623971"
