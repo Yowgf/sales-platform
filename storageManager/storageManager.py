3facb5380eac37ed6e8d95684d957c285aa793c7
Alexander Thomas Mol Holmquist <48073242+Yowgf@users.noreply.github.com>
2021-12-02 23:31:12 -0300

    Add comment and set status funcs, refactor (#4)

--- storageManager/storageManager.py
+++ storageManager/storageManager.py
@@ -8,6 +8,8 @@ having to import an internal package from the UI.
 from uuid import uuid4
 
 from .case import case
+from .case.caseStatus import caseStatus
+from .user import user
 
 class storageManager:
     # TODO: make users and cases data persistent in a database -aholmquist 2021-11-29
@@ -16,44 +18,31 @@ class storageManager:
         
         self.userType_customer = "customer"
         self.userType_sales = "sales"
-        self.userTypes = [self.userType_customer, self.userType_sales]
+        self.userType_all = "all" # Artificial wildcard user type
+        self.userTypes = [self.userType_customer, self.userType_sales, self.userType_all]
         
         self.users = {
-            "daniel@email.com": {
-                "name": "Daniel",
-                "type": "customer",
-                "password": "password321",
-            },
-            "joseph@email.com": {
-                "name": "Joseph",
-                "type": "sales",
-                "password": "password123",
-            },
-            "c": {
-                "name": "c",
-                "type": "customer",
-                "password": "c",
-            },
-            "s": {
-                "name": "s",
-                "type": "sales",
-                "password": "s",
-            },
+            "daniel@email.com": user("daniel@email.com", "Daniel", "customer", "password321"),
+            "joseph@email.com": user("joseph@email.com", "Joseph", "sales", "password123"),
+            "c": user("c", "c", "customer", "c"),
+            "s": user("s", "s", "sales", "s"),
         }
         
         self.currentUser = None
-
 
     # login returns True if login succeded, or False otherwise.
     def login(self, email, password):
         if (
             email in self.users.keys() and
-            self.users[email]["password"] == password
+            self.users[email].password == password
         ):
             self.currentUser = self.users[email]
             return True
         else:
             return False
+        
+    def logout(self):
+        self.currentUser = None
 
 ################################################################
 # Utility functions
@@ -76,11 +65,30 @@ class storageManager:
         self.cases[caseId] = c
         return c
             
-    def addCaseComment(self, caseId, comment):
-        self.cases[caseId].addComment(comment)
+    def addCaseComment(self, case, comment):
+        case.addComment(self.currentUser, comment)
 
-    def getCasesByCurrentUser(self):
-        return [self.cases[caseId]
-                for caseId in self.cases.keys()
-                if self.cases[caseId].createdBy == self.currentUser]
+    def getCasesByUserType(self, userType):
+        if userType == self.userType_all:
+            return [self.cases[caseId]
+                    for caseId in self.cases.keys()]
+        if userType == self.userType_customer:
+            return [self.cases[caseId]
+                    for caseId in self.cases.keys()
+                    if self.cases[caseId].createdBy == self.currentUser]
+        elif userType == self.userType_sales:
+            return [self.cases[caseId]
+                    for caseId in self.cases.keys()
+                    if self.cases[caseId].assignedTo == self.currentUser]
+        else:
+            raise ValueError("Unsupported user type {}".format(userType))
+
+    def assignCaseTo(self, case, salesUser):
+        case.assignTo(salesUser)
+        
+    def getAllCaseStatuses(self):
+        return caseStatus().allStatuses
+    
+    def setCaseStatus(self, case, status):
+        return case.status.set(status)
         
