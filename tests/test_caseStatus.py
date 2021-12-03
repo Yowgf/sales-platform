c9ca3bd3220695781c71f835485397bce9f456e7
Breno Pimenta <brenocsp@gmail.com>
2021-12-03 16:33:34 -0300

    Update test_caseStatus.py

--- tests/test_caseStatus.py
+++ tests/test_caseStatus.py
@@ -8,36 +8,36 @@ class TestCaseStatus:
         newCase = caseStatus()
         return newCase
     
-    def test_DefaultStatusIsPendingAnalysis():
+    def test_DefaultStatusIsPendingAnalysis(self):
         newCase = TestCaseStatus.create_newCase()
         assert newCase.defaultStatus == "pending analysis"
 
-    def test_DefaultStatusIsPendingAnalysisOnInitialization():
+    def test_DefaultStatusIsPendingAnalysisOnInitialization(self):
         newCase = TestCaseStatus.create_newCase()
         assert newCase.status == "pending analysis"
 
-    def test_CanSetStatusToPendingResponse():
+    def test_CanSetStatusToPendingResponse(self):
         newCase = TestCaseStatus.create_newCase()
         newCase.set(newCase.pendingResponse)
         assert newCase.status == "pending response"
 
-    def test_CanSetStatusToUnderAnalysis():
+    def test_CanSetStatusToUnderAnalysis(self):
         newCase = TestCaseStatus.create_newCase()
         newCase.set(newCase.underAnalysis)
         assert newCase.status == "under analysis"
         
-    def test_CanSetStatusToResolved():
+    def test_CanSetStatusToResolved(self):
         newCase = TestCaseStatus.create_newCase()
         newCase.set(newCase.resolved)
         assert newCase.status == "resolved"
     
-    def test_InvalidCaseSetRaiseException():
+    def test_InvalidCaseSetRaiseException(self):
         with pytest.raises(Exception):
             newCase = TestCaseStatus.create_newCase()
             invalidCase = "waiting for response"
             newCase.set(invalidCase)
 
-    def test_GetReturnCurrentStatus():
+    def test_GetReturnCurrentStatus(self):
         newCase = TestCaseStatus.create_newCase()
         newCase.set("resolved")
         assert newCase.get() == "resolved"
