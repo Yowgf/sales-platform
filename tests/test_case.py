ef49e7057fc165beebe2018987092805cb01f737
Breno Pimenta <brenocsp@gmail.com>
2021-12-03 18:46:03 -0300

    final tests for the case class

--- tests/test_case.py
+++ tests/test_case.py
@@ -1,3 +1,4 @@
+from typing import ClassVar
 import pytest
 
 from utils import utils
@@ -6,10 +7,7 @@ from storageManager.case.case import case
 from storageManager.case.caseComment.caseComment import caseComment
 
 class TestCase:
-    def create_newCase():
-        newCase = case()
-        return newCase
-    
+
     def makeLongWord(size):
         return '1'*size
     
@@ -17,7 +15,6 @@ class TestCase:
         with pytest.raises(Exception):
             longTitle = TestCase.makeLongWord(30)
             case.checkTitle(longTitle)
-    
     
     def test_longCategoryRaiseException(self):
         with pytest.raises(Exception):
@@ -28,3 +25,49 @@ class TestCase:
         with pytest.raises(Exception):
             longDescription = TestCase.makeLongWord(1000)
             case.checkCategory(longDescription)
+
+    def test_NewCasesAreCreatedWithoutComments (self):
+        newCase = case()
+        assert len(newCase.comments) == 0
+    
+    def test_NewCasesAreCreatedWithNoPersonAssinged (self):
+        newCase = case()
+        assert newCase.assignedTo == None
+
+    def test_NewCasesAreCreatedWithNoTitle (self):
+        newCase = case()
+        assert newCase.title == None
+
+    def test_NewCasesAreCreatedWithNoCategory (self):
+        newCase = case()
+        assert newCase.category == None
+
+    def test_NewCasesAreCreatedWithNoDescription (self):
+        newCase = case()
+        assert newCase.description == None
+
+    def test_CanCreateCommentsOnCases (self):
+        newCase = case()
+        user1 = "Maria Clara"
+        comment1 = "Some comments..."
+        newCase.addComment(user1, comment1)
+
+        assert newCase.comments[0].user == user1, "User that created the comment is diffenrent than expected"
+        assert newCase.comments[0].comment == comment1, "Comment created is diffenrent than expected"
+    
+    def test_CanPopulateCases (self):
+        newCase = case()
+        title = "Some title..."
+        category = "Some category..."
+        description = "Some description..."
+        newCase.populate(title, category, description)
+
+        assert newCase.title == title, "Case title is diffenrent than expected"
+        assert newCase.category == category, "Case category is diffenrent than expected"
+        assert newCase.description == description, "Case description is diffenrent than expected"
+    
+    def test_CanAssignPersonToCase (self):
+        newCase = case()
+        person = "Zerima Asoit"
+        newCase.assignedTo(person)
+        assert newCase.assignedTo == person
