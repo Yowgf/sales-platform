284fa398fc0be5733c31c39746d8c2182c1b8572
Alexander Thomas Mol Holmquist <48073242+Yowgf@users.noreply.github.com>
2021-12-03 21:27:09 -0300

    Refactor tests, remove easyClass (#14)

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,25 +2,39 @@ import pytest
 from utils.utils import inRange
 from utils.utils import checkAttLenInRange
 
+from errors.InvalidLength import InvalidLength
+
 class TestInRange:
 
-    def test_isInRangeMid (self):
-        rng = [0, 10]
-        assert inRange(5, rng) is True
+    @pytest.fixture
+    def sampleRange(self):
+        return (0, 10)
+    
+    def test_isInRangeMid (self, sampleRange):
+        assert inRange(5, sampleRange) is True
 
-    def test_isInRangeLowerBound (self):
-        rng = [0, 10]
-        assert inRange(0, rng) is True
+    def test_isInRangeLowerBound (self, sampleRange):
+        assert inRange(0, sampleRange) is True
 
-    def test_isInRangeUpperBound (self):
-        rng = [0, 10]
-        assert inRange(10, rng) is True
+    def test_isInRangeUpperBound (self, sampleRange):
+        assert inRange(10, sampleRange) is True
 
-    def test_isNotInRange (self):
-        rng = [0, 10]
-        assert inRange(-1, rng) is False
-        assert inRange(11, rng) is False
+    def test_isNotInRange (self, sampleRange):
+        assert inRange(-1, sampleRange) is False
+        assert inRange(11, sampleRange) is False
 
 class TestCheckAttLenInRange:
-    with pytest.raises(Exception):
-        checkAttLenInRange("test value", 10, [0,5])
+    
+    @pytest.fixture
+    def sampleRange(self):
+        return (0, 10)
+    
+    def test_IsInRangeNoException(self, sampleRange):
+        try:
+            checkAttLenInRange("attribute", "1" * (sampleRange[1] - 1), sampleRange)
+        except InvalidLength:
+            assert False, "Should not raise invalid length exception when in range"
+        
+    def test_IsNotInRangeRaisesException(self, sampleRange):
+        with pytest.raises(InvalidLength):
+            checkAttLenInRange("attribute", "1" * (sampleRange[1] + 1), sampleRange)
