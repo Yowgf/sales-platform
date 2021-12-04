9e155c71d302592ffa803fbe042ed8951e1e40f3
yowgf <alexthomasmol@gmail.com>
2021-12-03 21:25:23 -0300

    Refactor tests, remove easyClass

--- tests/test_InvalidLength.py
+++ tests/test_InvalidLength.py
@@ -1,6 +1,12 @@
 import pytest
 from errors.InvalidLength import InvalidLength
 
-class TestInvalidLengthExeceptionRaised:
-    with pytest.raises(Exception):
-        raise InvalidLength("10", [0,1])
+class TestInvalidLengthException:
+    
+    @pytest.fixture
+    def sampleInvalidException(self):
+        return InvalidLength("attribute", (0, 1))
+    
+    def test_InvalidLengthExceptionContainsGivenParameters(self, sampleInvalidException):
+        for subStr in ["attribute", "(0, 1)"]:
+            assert subStr in sampleInvalidException.__str__()
--- tests/test_caseComment.py
+++ tests/test_caseComment.py
@@ -6,15 +6,15 @@ from storageManager.case.caseComment.caseComment import caseComment
 class TestCaseComment:
     datetime = utils.datetime.now()
 
-    def create_newCommmet():
-        newComment = caseComment("Leona Vanessa", "Recomendo bastante", TestCaseComment.datetime)
-        return newComment
+    @pytest.fixture
+    def newComment(self):
+        return caseComment("Leona Vanessa", "Recomendo bastante", TestCaseComment.datetime)
 
-    def test_CanDefineCreatedBy (self):
-        assert TestCaseComment.create_newCommmet().createdBy == "Leona Vanessa"
+    def test_CanDefineCreatedBy (self, newComment):
+        assert newComment.createdBy == "Leona Vanessa"
 
-    def test_CanDefineComment (self):
-        assert TestCaseComment.create_newCommmet().comment == "Recomendo bastante"
+    def test_CanDefineComment (self, newComment):
+        assert newComment.comment == "Recomendo bastante"
 
-    def test_CanDefineCommentDatetime (self):
-        assert TestCaseComment.create_newCommmet().createdAt == TestCaseComment.datetime
+    def test_CanDefineCommentDatetime (self, newComment):
+        assert newComment.createdAt == TestCaseComment.datetime
