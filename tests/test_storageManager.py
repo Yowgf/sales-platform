262b8f26a1250bbff18fca26c938c85ff7f3388b
Breno Pimenta <brenocsp@gmail.com>
2021-12-11 17:50:31 -0300

    Update test_storageManager.py

--- tests/test_storageManager.py
+++ tests/test_storageManager.py
@@ -14,9 +14,13 @@ class TestStorageManager:
         return storageManager(sampleConfig())
 
     # Unit tests
-    def test_init(self, sampleSM):
+    def test_initUserType_customer(self, sampleSM):
         assert sampleSM.userType_customer == "customer"
+
+    def test_initUserType_sales(self, sampleSM):
         assert sampleSM.userType_sales == "sales"
+        
+    def test_initUserType_all(self, sampleSM):
         assert sampleSM.userType_all == "all"
 
     def test_assignCaseTo(self, sampleSM):
@@ -37,6 +41,15 @@ class TestStorageManager:
         sampleSM.setRate(scase, 4)
         assert scase.rate.val == 4
         assert suser.averageRate == 4
+    
+    def test_wrongLogin(self, sampleSM):
+        assert sampleSM.login("wrong email", "wrong password") == False
+    
+    def test_saleUserAdmin(self, sampleSM):
+        assert sampleSM.login("s", "s") == True
+    
+    def test_clientUserAdmin(self, sampleSM):
+        assert sampleSM.login("c", "c") == True
 
 # Integration tests. These require that 'config.yaml' exists and is configured
 # properly. Also, there has to be a live database on the configured endpoint.
